52a28bf1ae846c3e610f921b0574a55dee537264
Alexis Lopez <amlopez5841@eagle.fgcu.edu>
2022-10-13 18:04:08 -0400

    added remote shutdown functionality to bot

--- discord-script.py
+++ discord-script.py
@@ -13,7 +13,14 @@ async def on_ready():
 # example of bot command (if you write !ping in chat it will return Pong)
 @bot.command()
 async def ping(ctx): # ctx is content or context
-    await ctx.send("Pong")
+    await ctx.send("pong")
+
+@bot.command()
+async def shutdown(ctx):
+    await ctx.send("bot is offline")
+    await bot.close()
 
 # runs the bot using security token
-bot.run("__DISCORD_TOKEN__")    # replace with Discord token
+bot.run("MTAyOTk2NDAyODYwMjI0MTA3NQ.Gnk2JP.LButpXLS7A1pzOKNO8bWsdW7hJPTTWMugPdLSc")    # replace with Discord token
+
+
