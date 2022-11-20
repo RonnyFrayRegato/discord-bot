d500e42b2afaa44aa7ad173aa4e60dafb29d95b7
RonnyFray <ronnyfray@gmail.com>
2022-11-19 18:19:01 -0800

    Update workflow file

--- discord-bot.sh
+++ discord-bot.sh
@@ -4,3 +4,7 @@ DISCORD_TOKEN=${1}  # parameter is passed as an env through workflow file
 
 # run the Discord bot
 python discord-script.py ${DISCORD_TOKEN}
+
+sleep 60
+
+python discord-script.py exit
