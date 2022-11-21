a4a9a73d72816bc807d43d0835b2015828f2f4ad
Alexis Lopez <amlopez5841@eagle.fgcu.edu>
2022-11-20 20:23:29 -0500

    Remove slash commands Add prefix commands for basic functionalities

--- discord-script.py
+++ discord-script.py
@@ -1,79 +1,68 @@
 import random
+import sys
 import discord
 import scraping_google_images
+import database_creation
 from discord import app_commands
 from discord.ext import commands
-import database-creation
 
 
-class DiscordBot(discord.Client):
-    def __init__(self):
-        super().__init__(intents=discord.Intents.default())
-        self.synced = False
-
-    # bot syncs slash commands on startup
-    async def on_ready(self):
-        await tree.sync(guild=discord.Object(server_id))  # causes slash commands to refresh on bot startup
-        self.synced = True
-        print("Bot is online")
-        database-creation.connect_to_db()
+client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 # Global variables
-bot = DiscordBot()
-tree = app_commands.CommandTree(bot)
 server_id = 1030163730538954853
 
 
-@bot.event
+@client.event
+async def on_ready():
+    print("Bot is online")
+    #database_creation.connect_to_db()
+
+
+@client.event
 async def on_member_join(member):
     channel = member.guild.system_channel  # get system channel
     await channel.send(f"{member.mention} Welcome to the Server!")
 
 
-@bot.event
+@client.event
 async def on_member_remove(member):
     channel = member.guild.system_channel
     await channel.send(f"Goodbye {member.mention}!")
 
 
-@tree.command(name="hello", description="says a greeting")
-async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Hey {interaction.user.mention}, This is a slash command!")
+@client.command(name="hello")
+async def hello(ctx):
+    await ctx.reply(f"Hey {ctx.author.mention}!")
 
 
 # shows latency between bot and discord server
-@tree.command(name="ping", description="pings the user", guild=discord.Object(server_id))
-async def self(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Pong! Latency: {round(bot.latency * 1000)}ms")
+@client.command(name="ping")
+async def ping(ctx):
+    await ctx.reply(f"Pong! Latency: {round(client.latency * 1000)}ms")
 
 
 # gets a user question and replies with a random response.
-@tree.command(name="eightball", description="fun game", guild=discord.Object(server_id))
-async def self(interaction: discord.Interaction, question: str):
+@client.command(aliases=["8ball", "8b"])
+async def eightball(ctx, *, question):
     responses = ["Wow that's a bad idea", "Come on dude, that's ridiculous", "Better not tell you now", "Yes", "No",
-                 "Signs point to yes"
+                 "Signs point to yes",
                  "Don't count on it on your life", "Without a doubt", "homie, you must be nuts", "It is certain",
                  "You may rely on it", "Cannot predict now",
                  "The magic conch says no", "The magic conch says yes", "Most likely", "Better not tell you now.",
-                 "My reply is no", "My sources say no",
-                 "Very doubtful"]
-    await interaction.response.send_message(f"**Question:** {question}\n**Answers:** {random.choice(responses)}")
+                 "My reply is no", "My sources say no", "Very doubtful"]
+    await ctx.send(f":8ball: Question: {question}\n:8ball: Answer: {random.choice(responses)}")
 
 
-@tree.command(name="shutdown", description="turn the bot off", guild=discord.Object(server_id))
-async def self(interaction: discord.Interaction):
-    print("Bot is offline")
-    await interaction.response.send_message("Bot is offline")
-    await tree.client.close()
+@client.command(alias="shutdown")
+async def shutdown(ctx):
+    await ctx.send("Bot is offline")
+    await client.close()
 
 
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-
-
-@bot.command()
+@client.command()
 async def image(ctx):
     await scraping_google_images.main(ctx)
 
-
 # runs the bot using security token
-bot.run("__DISCORD_TOKEN__")
+client.run("DISCORD_TOKEN")
