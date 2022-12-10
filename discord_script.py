8d0629b39619323c3f1c7e98601203efd71aa460
Alexis Lopez <74120068+AlexisMLopez@users.noreply.github.com>
2022-12-10 17:24:27 -0500

    Add moderation functionality (#1)
    * added remote shutdown functionality to bot
    * removed discord token
    * Add kick command
    * Add ban command
    * Add clear command

--- discord_script.py
+++ discord_script.py
@@ -3,7 +3,6 @@ import random
 import discord
 import scraping_google_images
 import database_creation
-from discord import app_commands
 from discord.ext import commands
 
 client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
@@ -19,6 +18,7 @@ async def on_ready():
     database_creation.init_table()
 
 
+# client events
 @client.event
 async def on_member_join(member):
     channel = member.guild.system_channel  # get system channel
@@ -33,6 +33,40 @@ async def on_member_remove(member):
     await channel.send(f"Goodbye {member.mention}!")
 
 
+@client.event
+async def on_message(message):
+    database_creation.increase_user_msg_count(message.author.id)
+    await client.process_commands(message)
+
+
+# moderation commands
+@client.command()
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, member: discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+    await ctx.send(f'{member} has been kicked from the server.')
+
+
+@client.command()
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, member: discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+    await ctx.send(f'{member} has been banned from the server.')
+
+
+@client.command()
+@commands.has_permissions(manage_messages=True)
+@commands.cooldown(1, 5, commands.BucketType.user)
+async def clear(ctx, amount=6):
+    amount = amount + 1
+    if amount > 101:
+        await ctx.send("Cannot delete more than 100 messages.")
+    else:
+        await ctx.channel.purge(limit=amount)
+        await ctx.send(f"Cleared {amount - 1} messages.")
+
+
+# basic commands
 @client.command(name="hello")
 async def hello(ctx):
     await ctx.reply(f"Hey {ctx.author.mention}!")
@@ -47,9 +81,7 @@ async def mycount(ctx):
 # shows latency between bot and discord server
 @client.command(name="ping")
 async def ping(ctx):
-
     await ctx.reply(f"Pong! Latency: {round(client.latency * 1000)}ms")
-
 
 
 # gets a user question and replies with a random response.
@@ -73,15 +105,6 @@ async def shutdown(ctx):
 @client.command()
 async def image(ctx):
     await scraping_google_images.main(ctx)
-
-
-@client.event
-async def on_message(message):
-
-    database_creation.increase_user_msg_count(message.author.id)
-    await client.process_commands(message)
-
-
 
 
 # runs the bot using security token
